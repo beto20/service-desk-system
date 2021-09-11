64bf7a641f532a2a50b5a2e0d7c938b49c508455
Alberto Valentin Velasquez Santos <avelassa@everis.com>
2021-09-11 13:42:37 -0500

    maven fixed

---
+++ Back-end/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+U
+%ms-spring-eureka/ms-spring-eureka.iml,b\4\b4b3f5acb78207a54c964769d739db03ba6addfc
---
+++ Back-end/ms-spring-cloud-gateway/src/main/java/com/alberto/ms/spring/cloud/gateway/MsSpringCloudGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.alberto.ms.spring.cloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSpringCloudGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsSpringCloudGatewayApplication.class, args);
+    }
+
+}
