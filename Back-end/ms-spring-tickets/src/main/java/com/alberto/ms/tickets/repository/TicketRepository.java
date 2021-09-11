84a4b69992cc9cf779b4d243c85a0fa27d847a6f
Alberto Valentin Velasquez Santos <avelassa@everis.com>
2021-09-11 10:09:59 -0500

    frontend features

--- Back-end/ms-spring-tickets/src/main/java/com/alberto/ms/tickets/repository/TicketRepository.java
+++
@@ -1,9 +0,0 @@
-package com.alberto.ms.tickets.repository;
-
-import com.alberto.ms.tickets.model.domain.Ticket;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TicketRepository extends ReactiveCrudRepository<Ticket, Integer> {
-}
