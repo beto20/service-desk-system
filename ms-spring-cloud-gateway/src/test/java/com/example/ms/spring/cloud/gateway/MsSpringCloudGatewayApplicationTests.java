09f028330ae296c52edd37c744b3af6395e775c4
Alberto Valentin Velasquez Santos <avelassa@everis.com>
2021-09-11 09:31:02 -0500

    add gateway microservice

---
+++ ms-spring-cloud-gateway/src/test/java/com/example/ms/spring/cloud/gateway/MsSpringCloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ms.spring.cloud.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsSpringCloudGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
