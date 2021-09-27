2c8ce5ac5debec1cf8a243828fe2137a4b1410b4
Alberto Valentin Velasquez Santos <avelassa@everis.com>
2021-09-27 18:41:50 -0500

    new commit

--- Back-end/ms-spring-users/src/main/java/com/example/ms/spring/users/repository/DepartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.ms.spring.users.repository;
-
-import com.example.ms.spring.users.model.Department;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DepartmentRepository extends ReactiveMongoRepository<Department, String> {
-}
