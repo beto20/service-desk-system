68fc99c399419ee4504c9f6aa0c9ae2eb210a416
Alberto Valentin Velasquez Santos <avelassa@everis.com>
2021-09-27 19:35:29 -0500

    delete frontend file

--- Front-end/src/app/classes/ticket.ts
+++
@@ -1,9 +0,0 @@
-export class ticket{
-    userid! : number;
-    description! : string;
-    category! : string;
-    date! : Date;
-    priority! : string;
-    status! : string;
-    user_id! : number;
-}
